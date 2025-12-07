1ba15649f7487ba1640f145e5d8580fcfd125887
Carlos Rodriguez <carlosarodriguez@students.utech.edu.jm>
2025-12-07 00:44:39 -0500

    Refactor cart functionality and improve formatting

--- products.js
+++ products.js
@@ -1,15 +1,23 @@
+/*==================================
+  Products Functionality Handling JavaScript
+  ==================================*/
+
 // Load existing cart or create a new one
 let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
 // Function to add items to cart
-function addToCart(name, price) {
+function addToCart(name, price)
+{
     price = parseFloat(price);
 
     let item = cart.find(product => product.name === name);
 
-    if (item) {
+    if (item)
+        {
         item.quantity++;
-    } else {
+    }
+    else
+    {
         cart.push({
             name: name,
             price: price,
@@ -21,9 +29,10 @@ function addToCart(name, price) {
     alert(name + " added to cart!");
 }
 
-
-document.querySelectorAll(".add-cart").forEach(button => {
-    button.addEventListener("click", function () {
+document.querySelectorAll(".add-cart").forEach(button =>
+    {
+    button.addEventListener("click", function ()
+    {
         let name = this.getAttribute("data-name");
         let price = this.getAttribute("data-price");
         addToCart(name, price);
